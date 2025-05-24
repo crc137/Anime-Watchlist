8f9b5e0af5afacc677007622e8684998ba38ff4b
Alberto Clemente <165967398+crc137@users.noreply.github.com>
2025-05-24 12:46:17 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { createUser, updateAnimeList, getUser } from './utils/api';
 import Profile from './components/Profile';
